df9816b7b06df3f8d1751c79ff0c427c8cfb52dd
David Stein <dstein@flatironinstitute.org>
2021-11-02 17:01:31 -0400

    updated with second example and two-color plots for different circulations

--- python/simulate.py
+++ python/simulate.py
@@ -17,16 +17,28 @@ from generate_initial_data import get_initial_data
 
 N = 1000             # number of particles
 r = 0.3              # radius of ensemble
-kernel = 'euler'     # kernel to use ('euler', 'qsg', 'sd')
-sd_number = 0.5      # saffman-delbruck number (only used if kernel=='sd')
-steric = True        # whether to include steric interactions
 rSteric = 0.01       # radius where steric interactions turn on (only used if steric==True)
 wSteric = 1000000.0  # strength of steric interactions (only used if steric==True)
 random_seed = -1     # random seed for setting initial conditions (set to -1 to not set a seed)
-omega1 = 1           # size of circulation for first half of the particles
-omega2 = 1           # size of ciculation for second half of the particles
-max_time = 0.05      # time to timestep to
 error_tol = 1.0e-6   # error tolerance for DOP853 Timestepper
+sd_number = 1.0      # saffman-delbruck number (only used if kernel=='sd')
+
+
+if True: # QSG + steric, same circulation 
+
+	kernel = 'sd'        # kernel to use ('euler', 'qsg', 'sd')
+	steric = True        # whether to include steric interactions
+	omega1 = 1           # size of circulation for first half of the particles
+	omega2 = 1           # size of ciculation for second half of the particles
+	max_time = 0.1       # time to timestep to
+
+else: # Euler, no steric, different circulations
+
+	kernel = 'euler'     # kernel to use ('euler', 'qsg', 'sd')
+	steric = False        # whether to include steric interactions
+	omega1 = 0.1         # size of circulation for first half of the particles
+	omega2 = 10          # size of ciculation for second half of the particles
+	max_time = 0.01      # time to timestep to
 
 ################################################################################
 # construct vector for circulations
@@ -87,6 +99,12 @@ Hs = []
 ts = []
 X0 = []
 Y0 = []
+# index for first slow particle
+zero_x_index = 0
+zero_y_index = N
+if omega1 > omega2:
+	zero_x_index += N
+	zero_y_index += N
 while stepper.status != 'finished':
 	stepper.step()
 	H = compute_hamiltonian(stepper.y)
@@ -95,8 +113,8 @@ while stepper.status != 'finished':
 	sys.stdout.flush()
 	Hs.append(H)
 	ts.append(stepper.t)
-	X0.append(stepper.y[0])
-	Y0.append(stepper.y[N])
+	X0.append(stepper.y[zero_x_index])
+	Y0.append(stepper.y[zero_y_index])
 
 ################################################################################
 # analysis
@@ -115,20 +133,35 @@ x, y = divide_state(X)
 # plot the initial and final configurations
 fig, [ax0, ax1] = plt.subplots(1, 2, figsize=(12,6))
 ax0.scatter(initial_x, initial_y, color='blue')
-ax1.scatter(x, y, color='red')
+if omega1 == omega2:
+	ax1.scatter(x, y, color='red')
+else:
+	ax1.scatter(x[:halfN], y[:halfN], color='red', label="Fast" if omega1 > omega2 else "Slow")
+	ax1.scatter(x[halfN:], y[halfN:], color='green', label="Fast" if omega1 < omega2 else "Slow")
+	ax1.legend()
 ax0.set_title('Initial configuration')
 ax1.set_title('Final state')
 for ax in [ax0, ax1]:
 	ax.set_aspect('equal')
 
 # compute the structure factor
-Sq, rSq, rv = compute_structure_factor(x, y, r)
+if omega1 == omega2:
+	Sq, rSq, rv = compute_structure_factor(x, y, r)
+else:
+	if omega1 > omega2:
+		xf, yf = x[:halfN], y[:halfN]
+	else:
+		xf, yf = x[halfN:], y[halfN:]
+	Sq, rSq, rv = compute_structure_factor(xf, yf, r)
 
 # plot the structure factor
 fig, [ax0, ax1] = plt.subplots(1, 2, figsize=(12,6))
 ax0.imshow(Sq)
 ax0.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[])
-ax0.set(title='Structure factor')
+title = 'Structure factor'
+if omega1 != omega2:
+	title += ' (of faster particles)'
+ax0.set(title=title)
 ax1.plot(rv, rSq, color='black', linewidth=3)
 ax1.set(xscale='log', yscale='log')
 ax1.set(xlabel=r'$q$', ylabel=r'$\langle S(q)\rangle$')
@@ -138,7 +171,10 @@ ax1.set(title='Radially averaged structure factor')
 fig, [ax0, ax1] = plt.subplots(1, 2, figsize=(12,6))
 ax0.plot(X0, Y0, color='black')
 ax0.set(xlabel=r'$x$', ylabel=r'$y$')
-ax0.set(title='Single-rotor trajectory')
+title = 'Single-rotor trajectory'
+if omega1 != omega2:
+	title += ' (of slow particle)'
+ax0.set(title=title)
 HE = np.abs(np.array(Hs)-H0)/np.abs(H0)
 ax1.plot(ts, HE, color='black', linewidth=3)
 ax1.set(title='Relative error in the Hamiltonian')
